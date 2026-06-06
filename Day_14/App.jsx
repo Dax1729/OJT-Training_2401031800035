37a2ad1b7b886050823d3ad2b80ef3aa9327446e
Dax <kharavadax@gmail.com>
2026-06-06 12:42:10 +0530

    Create App component with Header, Content, and Footer

---
+++ Day_14/App.jsx
@@ -0,0 +1,18 @@
+import Header from './Components/header';
+// import Navbar from './Components/navbar';
+import Content from './Components/hero_content';
+import Footer from './Components/footer';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <Navbar /> */}
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
